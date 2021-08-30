53b95979616915ab808010d5baac8fa5af1a7fff
tana9373 <83992274+tana9373@users.noreply.github.com>
2021-08-30 17:34:25 +0530

    Update dev.py

--- GroupMenter/modules/dev.py
+++ GroupMenter/modules/dev.py
@@ -90,5 +90,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "⚜ DEVS ⚜"
+__mod_name__ = "😆TANAJI😆"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
