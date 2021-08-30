fbf4381aa11e6314ed39505f65f4011b1fae6fda
tana9373 <83992274+tana9373@users.noreply.github.com>
2021-08-30 22:37:07 +0530

    Update _pyrogithub.py

--- GroupMenter/modules/_pyrogithub.py
+++ GroupMenter/modules/_pyrogithub.py
@@ -5,7 +5,7 @@ from GroupMenter import pbot
 from GroupMenter.pyrogramee.errors import capture_err
 
 
-__mod_name__ = "GITHHUB"
+__mod_name__ = "📜GITHHUB📜"
 
 __help__ = """
 I will give information about github profile 
