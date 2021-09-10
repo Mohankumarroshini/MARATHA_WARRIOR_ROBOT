5859c81d30536439b3b5c04f942c71731df67d84
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-09-10 20:49:05 +0530

    Update misc.py

--- GroupMenter/modules/helper_funcs/misc.py
+++ GroupMenter/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("👨‍💻DEVELOPER👨‍💻", url="t.me/XD_PERSON")]]
+        pairs += [[EqInlineKeyboardButton("👨‍💻DEVELOPER👨‍💻", url="t.me/My_Dear_lightbright")]]
 
     return pairs
 
